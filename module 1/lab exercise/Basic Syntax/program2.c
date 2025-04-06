e35a3a5f1f32a8e31bf2e39aa6695e5a6e57cad9
garimaprajapati1799 <prajapatigarima381@gmail.com>
2025-04-06 17:57:14 +0530

    datatypes practice

---
+++ module 1/lab exercise/Basic Syntax/program2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int x=5,y=10,z=15;
+float f=85.69;
+char ch='$';
+double d=89.45879634;
+printf("Value of x= %d",x);
+printf("\nValue of y= %d",y);
+printf("\nValue of z= %d",z);
+printf("\nValue of f= %.2f",f);
+printf("\nValue of ch= %c",ch);
+printf("\nValue of d= %lf",d);
+return 0;
+}
