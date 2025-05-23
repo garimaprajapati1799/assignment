fc7a03e40a503de4553f68e26f93763b2e7c0e46
garimaprajapati1799 <prajapatigarima381@gmail.com>
2025-05-23 17:27:25 +0530

    submitted

---
+++ Module 3/2.Variables,Datatypes and Operators/2.Type conversion.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x = 10;
+	float y = x;
+	cout<<"Value of x : "<<x<<endl;
+	cout<<"Value of y : "<<y<<endl;
+	
+	int a=15;
+	int b=2;
+	cout<<"\nDivision is : "<<(float)a/(float)b;
+	return 0;
+}
