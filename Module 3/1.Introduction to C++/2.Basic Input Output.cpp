baa8e4f485fd139adb057c589bcaa2ad1c5a1d94
garimaprajapati1799 <prajapatigarima381@gmail.com>
2025-05-23 21:17:05 +0530

    submitted

---
+++ Module 3/1.Introduction to C++/2.Basic Input Output.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	string name;
+	cout<<"\nEnter your age : ";
+	cin>>age;
+	cout<<"\nEnter your name : ";
+	cin>>name;
+	cout<<"\nYour name is "<<name<<" and you are "<<age<<" year old!";
+	return 0; 
+}
---
+++ Module 3/1.Introduction to C++/4.Setting up developmentEnvironment.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n1,n2;
+	cout<<"\nEnter the first number : ";
+	cin>>n1;
+	cout<<"\nEnter the second number : ";
+	cin>>n2;
+	cout<<"\nThe sum of "<<n1<<" and "<<n2<<" is : "<<n1+n2;
+	return 0;
+}
